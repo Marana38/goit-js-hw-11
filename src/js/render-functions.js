2a77d101cfbfdb0373fd75d0505af99d1d0c53f2
Marana38 <maranagor10@gmail.com>
2024-11-17 09:17:51 +0200

    mod css11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -15,10 +15,10 @@ export function createImageMarkup(images) {
           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         </a>
         <div class="info">
-          <p><b>Likes</b>: ${likes}</p>
-          <p><b>Views</b>: ${views}</p>
-          <p><b>Comments</b>: ${comments}</p>
-          <p><b>Downloads</b>: ${downloads}</p>
+          <p><b>Likes</b> ${likes}</p>
+          <p><b>Views</b> ${views}</p>
+          <p><b>Comments</b> ${comments}</p>
+          <p><b>Downloads</b> ${downloads}</p>
           
         </div>
       </div>
